d1bd6d9d383b5d7c669d7f50b21c33b6581aa996
hyunmang2 <wowow19811@gmail.com>
2024-11-09 14:43:57 +0900

    spring security 일부기능 제외 설정

--- src/main/java/com/sparta/onboarding/OnboardingApplication.java
+++ src/main/java/com/sparta/onboarding/OnboardingApplication.java
@@ -2,8 +2,9 @@ package com.sparta.onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class OnboardingApplication {
 
     public static void main(String[] args) {
