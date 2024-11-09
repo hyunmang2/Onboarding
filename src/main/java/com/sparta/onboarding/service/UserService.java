26c50365934b14970b47b42b2c6e0a7def5ddbb8
hyunmang2 <wowow19811@gmail.com>
2024-11-10 02:00:32 +0900

    유저 이름 찾는 메서드 추가, 로그인 메서드 반환타입 변경

--- src/main/java/com/sparta/onboarding/service/UserService.java
+++ src/main/java/com/sparta/onboarding/service/UserService.java
@@ -1,15 +1,26 @@
 package com.sparta.onboarding.service;
 
+import com.sparta.onboarding.dto.LoginRequestDto;
+import com.sparta.onboarding.dto.LoginResponseDto;
 import com.sparta.onboarding.dto.SignupRequestDto;
+import com.sparta.onboarding.entity.User;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
 
     // 회원가입
     void signup(SignupRequestDto signupRequestDto);
 
+    // 로그인
+    LoginResponseDto login(LoginRequestDto loginRequestDto, HttpServletResponse response);
+
     // 유저이름 확인
     void validUsername(String username);
 
     // 비밀번호 확인
     void validPassword(String password);
+
+    // 유저이름 찾아오기
+    User getUsername(String username);
+
 }
